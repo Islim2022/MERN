de7428bb8a99186697ec09021e25c6ac70a3d0a5
Hanei Islim <hanei_islim@hotmail.com>
2022-06-05 15:13:48 +0300

    First assignment

---
+++ Fundamentals/Hoist.js
@@ -0,0 +1,18 @@
+// 1.
+// console.log(hello);                                   
+// var hello = 'world';                                 
+var hello;
+hello = 'world';
+console.log(hello);
+
+// 2.
+var needle = 'haystack';
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+
+
+
+    
